af544f7aee769c95e9257b9935fb67f88e5cf06f
John Harvard <fabiocosta0305@gmail.com>
2013-01-26 19:24:20 -0200

    * PHP:
    dataSent() command isolated;
    
    implemented /secret command;
    
    * CSS:
    
    new classes for secret info;

--- html/chat.php
+++ html/chat.php
@@ -1,9 +1,9 @@
 <?php
 
+// configuration
+require("../includes/config.php"); 
 require_once("../includes/diceRoller.php");
-
-    // configuration
-    require("../includes/config.php"); 
+require_once("../includes/dataSent.php");
 
     // if form was submitted
     if ($_SERVER["REQUEST_METHOD"] == "POST")
@@ -43,85 +43,6 @@ render ("chatWindow.php", ["title" => "Table",
                            "extraJS"=> ["js/chat.js"]]);
     }
 
-function dataSent($data, $user)
-{
-    $query="";
-    $myData=$data;
-    $command="";
-    $parm="";
-        
-    $dataQuery=query("select UNIX_TIMESTAMP(CURRENT_TIMESTAMP) as last");
-
-    if ($dataQuery === false)
-        return (false);
-
-    $lastTimestamp=$dataQuery[0]['last'];
-
-    // Now some parsing...
-
-    if ($data[0]==="/")
-    {
-        $info=explode(" ",$myData);
-
-        $command=$info[0];
-
-        $info[0]=" ";
-        $myData=implode(" ",$info);
-        
-        switch($command)
-        {
-            case '/off':
-                $info[0]="";
-                $myData=implode(" ",$info);
-                $data=query("insert into offChatLog (userid,text) VALUES (?,?)",
-                            $user, $myData); 
-                break;
-
-            case '/me':
-                $info[0]="";
-                $myData=implode(" ",$info);               
-                break;
-                
-            case '/defaultDice':
-                $info[0]="";
-                $myData=implode(" ",$info);
-
-                $parm=$info[1];
-
-                $numberOfMatches=preg_match("/^([0-9]+)?d([0-9fF]+)([+-][0-9]+)?$/",$info[1]);
-                
-                if ($numberOfMatches == 0)
-                    $myData="Error: entry $die not valid";
-                else
-                {
-                    $_SESSION['defaultDice']=$info[1];
-                    $myData="set default dice to ".$info[1];
-                }
-                break;
-
-            case '/dice':
-                $parm=$info[1];
-                $info[0]=$info[1]="";
-                          
-                if (!isset($parm) || empty($parm))
-                    if (isset($_SESSION['defaultDice'])) $parm=$_SESSION['defaultDice'];
-                
-                $myData=rollDice($parm);
-                break;
-
-            default:
-                $myData="had tried a invalid command:".$command;
-                $command="/error";
-        }
-    }
-    
-    /* $data=query($query, $user, $myData); */
-    $data=query("insert into onChatLog (userid,text,command,parm) VALUES (?,?,?,?)",
-                $user, $myData, $command, $parm); 
-   
-    return($lastTimestamp);
-}
-
 function offChatGet($lastTimestamp, $user)
 {
     $chatData="";
@@ -147,7 +68,7 @@ function onChatGet($lastTimestamp, $user)
     $chatData="";
     $timestamp=$lastTimestamp;
     $dataQuery=query("
-select username,text,unix_timestamp(postedOn) as postedOn,command,parm
+select  userid,username,text,unix_timestamp(postedOn) as postedOn,command,parm
   from user,onChatLog
  where user.id=onChatLog.userid
    and unix_timestamp(postedOn) > ? order by postedOn",$lastTimestamp);
@@ -161,9 +82,27 @@ select username,text,unix_timestamp(postedOn) as postedOn,command,parm
         {
             case '/off':
                 break;
+                
+            case '/secretDice':
+                if ($line['userid']===$_SESSION['id'])
+                    $chatData.="<div class=chatInfo>".$line['username']." rolled ".$line['parm']." with a ".$line['text']."</div>";
+                else
+                    $chatData.="<div class=chatInfo>".$line['username']." rolled ".$line['parm']."</div>";
+                break;
+
+            case '/fakeDice':
+                $chatData.="<div class=chatInfo>".$line['username']." rolled ".$line['parm']."</div>";
+                break;
+                
             case '/dice':
                 $chatData.="<div class=chatInfo>".$line['username']." rolled ".$line['parm']." with a ".$line['text']."</div>";
                 break;
+
+            case '/secret':
+                if ($line['parm']==$_SESSION['id'])
+                    $chatData.="<div class=secretChatText><span class=secretChatUser>".$line['username']." (secret):</span> ".$line['text']."</div>";
+                break;
+                
             case '/defaultDice':
             case '/me':
             case '/error':
---
+++ includes/dataSent.php
@@ -0,0 +1,113 @@
+<?php
+
+function dataSent($data, $user)
+{
+    $query="";
+    $myData=$data;
+    $command="";
+    $parm="";
+        
+    $dataQuery=query("select UNIX_TIMESTAMP(CURRENT_TIMESTAMP) as last");
+
+    if ($dataQuery === false)
+        return (false);
+
+    $lastTimestamp=$dataQuery[0]['last'];
+
+    // Now some parsing...
+
+    if ($data[0]==="/")
+    {
+        $info=explode(" ",$myData);
+
+        $command=$info[0];
+
+        $info[0]=" ";
+        $myData=implode(" ",$info);
+        
+        switch($command)
+        {
+            case '/off':
+                $info[0]="";
+                $myData=implode(" ",$info);
+                $data=query("insert into offChatLog (userid,text) VALUES (?,?)",
+                            $user, $myData); 
+                break;
+
+            case '/me':
+                $info[0]="";
+                $myData=implode(" ",$info);               
+                break;
+                
+            case '/defaultDice':
+                $info[0]="";
+                $myData=implode(" ",$info);
+
+                $parm=$info[1];
+
+                $numberOfMatches=preg_match("/^([0-9]+)?d([0-9fF]+)([+-][0-9]+)?$/",$info[1]);
+                
+                if ($numberOfMatches == 0)
+                    $myData="Error: entry $die not valid";
+                else
+                {
+                    $_SESSION['defaultDice']=$info[1];
+                    $myData="set default dice to ".$info[1];
+                }
+                break;
+
+            case '/secret':
+                if (count($info)!==3)
+                {
+                    $command="/error";
+                    $myData="not enought parameters";
+                }
+                else
+                {
+                    $parm=$info[1];
+                    $info[0]=$info[1]="";
+                    
+                    $myData=implode(" ",$info);
+                    
+                    $data=query("select id from user where username=?",
+                                $parm);
+                    
+                    if ($data === false)
+                    {
+                        $command="/error";
+                        $myData="user $parm not found";
+                    }
+                    else
+                    {
+                        $row=$data[0];
+                        $parm=$row['id'];
+                    }
+                }               
+                break;
+
+            case '/fakeDice':
+            case '/secretDice':
+            case '/dice':
+                $parm=$info[1];
+                $info[0]=$info[1]="";
+                          
+                if (!isset($parm) || empty($parm))
+                    if (isset($_SESSION['defaultDice'])) $parm=$_SESSION['defaultDice'];
+                
+                $myData=rollDice($parm);
+                break;
+
+            default:
+                $myData="had tried a invalid command:".$command;
+                $command="/error";
+        }
+    }
+    
+    /* $data=query($query, $user, $myData); */
+    $data=query("insert into onChatLog (userid,text,command,parm) VALUES (?,?,?,?)",
+                $user, $myData, $command, $parm); 
+   
+    return($lastTimestamp);
+}
+
+?>
